7ce746f9daf38275a8c1acbceba663bef5ae79f2
Thomas Skjerping <skje02@gmail.com>
2025-05-23 11:54:27 +0200

    Remove 'vipps' from payment method types in checkout session creation

--- api/create-checkout-session.js
+++ api/create-checkout-session.js
@@ -7,7 +7,7 @@ module.exports = async (req, res) => {
   }
 
   const session = await stripe.checkout.sessions.create({
-    payment_method_types: ['card', 'vipps'],
+    payment_method_types: ['card'],
     line_items: [
       {
         price_data: {
